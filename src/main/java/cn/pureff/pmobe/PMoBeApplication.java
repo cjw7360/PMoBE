513f68f2e0e405a0bf237a7bd1fdb297c38a5582
clemon <chenjw@inhand.com.cn>
2020-09-11 10:33:22 +0800

    init

---
+++ src/main/java/cn/pureff/pmobe/PMoBeApplication.java
@@ -0,0 +1,12 @@
+package cn.pureff.pmobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PMoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PMoBeApplication.class, args);
+    }
+}
---
+++ src/test/java/cn/pureff/pmobe/PMoBeApplicationTests.java
@@ -0,0 +1,11 @@
+package cn.pureff.pmobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PMoBeApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
